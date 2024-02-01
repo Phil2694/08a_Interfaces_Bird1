85ff26ddf0eb8622768e04d92722ccbb4e48b69d
Phil2694 <155629796+Phil2694@users.noreply.github.com>
2024-02-01 09:14:11 +0000

    BirdFeathers

--- com/cc/java/Bird.java
+++ com/cc/java/Bird.java
@@ -1,9 +1,14 @@
 package com.cc.java;
 
-public class Bird implements Flyable {
+public class Bird implements Flyable,Feathers {
 
     @Override
     public String fly() {
         return "I m a bird, I can fly!";
+    }
+
+    @Override
+    public String hasFeathers() {
+        return "Im a bird, I have feathers!";
     }
 }
---
+++ com/cc/java/Feathers.java
@@ -0,0 +1,7 @@
+package com.cc.java;
+
+public interface Feathers {
+
+    public abstract String hasFeathers();
+
+}
